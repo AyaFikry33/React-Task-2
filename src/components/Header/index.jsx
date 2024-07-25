e447377a8baf57678f4b6542799ece90b3da4c2d
MPH\G528607 <aya.ahmed@idemia.com>
2024-07-25 11:04:25 +0300

    Remove unnecessary code

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,8 @@
 
-import { useContext } from 'react';
-import ThemeContext from '../../Context/ThemeContext';
 import Theme from '../Theme';
 export default function Header () {
-    const {theme} = useContext(ThemeContext);
-
     return (
-        <header className={`header ${theme}`}>
+        <header className="header">
             <div className="header-content">
                 <h1>Where in the world?</h1>
                 <Theme/>
